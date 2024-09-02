3c3cd451ef4e0e23bff2f6c975b82a632a1e450c
wisye <lunarwisyendra@gmail.com>
2024-09-02 21:13:37 +0700

    added script for submission

---
+++ Bagian B/script.py
@@ -0,0 +1,38 @@
+import requests # type: ignore
+import pyotp # type: ignore
+import base64
+import json
+
+userid = '18222095'
+shared_secret = 'seleksister2418222095Furina'
+
+encoded_secret = base64.b32encode(shared_secret.encode()).decode()
+
+totp = pyotp.TOTP(encoded_secret, interval=30, digits=8, digest=pyotp.hashlib.sha256)
+password = totp.now()
+
+print(password)
+
+credentials = f"{userid}:{password}"
+encoded_credentials = base64.b64encode(credentials.encode()).decode()
+
+url = 'http://sister21.tech:7787/recruitment/submit/b'
+header = {
+	'Content-Type': 'application/json',
+	'Authorization': f'Basic {encoded_credentials}'
+}
+
+payload = {
+	"fullname" : "Wisyendra Lunarmalam",
+	"link" : "https://github.com/wisye/Seleksi-Sister-2024/tree/main/Bagian%20B",
+	"message" : "MILF (Man I Love Furina)"
+}
+
+response = requests.post(url, headers=header, data=json.dumps(payload))
+
+print(f"Status Code: {response.status_code}")
+print("Response Body:")
+print(response.text)
+
+print()
+print(response)
